ecebd5ed39a82adb898f173e2ccfca16b69e62fe
Rodrigo Flores <flores@hey.com>
2024-03-02 23:08:42 -0300

    :sparkles: Builded version

---
+++ README.md
@@ -0,0 +1,13 @@
+# 
+
+## To build
+
+```
+cargo build --target=wasm32-unknown-unknown --release
+```
+
+## To run
+
+```
+docker compose up --build
+```
--- src/lib.rs
+++ src/lib.rs
@@ -26,9 +26,24 @@ impl Context for RandomStringGenerator {}
 
 impl HttpContext for RandomStringGenerator {
     fn on_http_request_headers(&mut self, _:usize, _: bool) -> Action {
+        let piece = "OIOI5";
+//        let piece = &piece_s[..]
+
+        info!("Piece: {}", piece);
+
         for (name, value) in &self.get_http_request_headers() {
             info!("In WASM : #{} -> {}: {}", self.context_id, name, value);
             info!("Seed: {}", self.seed);
+        }
+        let current_header = self.get_http_request_header("X-Correlation-ID");
+
+        match current_header {
+            Some(cid) => {
+                self.set_http_request_header("X-Correlation-ID", Some(&(cid.to_string() + "." + piece)));
+            },
+            None => {
+                self.set_http_request_header("X-Correlation-ID", Some(piece));
+            }
         }
         Action::Continue
     }
